cc637845e0aa6b2e5dbf6d2086fc9c4c70a2e6f3
jdouglas21 <jhd5244@psu.edu>
2024-02-04 13:29:40 -0500

    Added Cards to the Site

--- src/my-card.js
+++ src/my-card.js
@@ -5,6 +5,8 @@ import { LitElement, html, css } from 'lit';
  * 1. 
  */
 
+
+
 export class MyCard extends LitElement {
 
   static get tag() {
@@ -14,6 +16,9 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.header = "The Header";
+    this.imageSrc = "default-image.jpg";
+    this.par = "A paragraph with words";
   }
 
   static get styles() {
@@ -25,12 +30,30 @@ export class MyCard extends LitElement {
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+      <div id="cardlist">
+    <div class="card">
+      <div class="card-header">
+        <h2>${this.header}</h2>
+      </div>
+      <img class="card-image" alt="Card Image" src="${this.imageSrc}"/>
+      <div class="card-text">
+        <h3 class="card-title">${this.title}</h3>
+        <div class="card-details">
+          <p>${this.par}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+      <div>${this.title}</div>`;
   }
 
   static get properties() {
     return {
+      header: { type:String },
       title: { type: String },
+      imageSrc: { type: String },
+      par: {type: String},
     };
   }
 }
