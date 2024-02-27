cafa82cbf5c32be856f8990b9414a0566c29235b
jhdouglas21 <jhd5244@psu.edu>
2024-02-27 12:53:44 -0500

    Adjusted Alert icon and added background with Details

--- src/alert-app.js
+++ src/alert-app.js
@@ -18,24 +18,26 @@ export class AlertApp extends LitElement {
                     <p>February, 2, 2024</p>
                     <p>12:00 AM</p>
                 </div>
-                <div class="alert-text">
-                    <span tabindex="0" class="alert-message">
-                        <div class="clearfix text-formatted field field--name-message field--type-text-long field--label-hidden field__item">
-                            <p>This is a default alert.&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 82" class="alert-icon">
+                                    <g transform="translate(-350.099 -428.714)">
+                                    <g transform="translate(350.099 428.714)" fill="none" stroke-width="6">
+                                    <circle cx="41" cy="41" r="41" stroke="none"></circle>
+                                    <circle cx="41" cy="41" r="38" fill="none"></circle></g>
+                                    <g transform="translate(384.41 448.566)"><rect width="10.381" height="7.786" transform="translate(0.919 34.336)"></rect>
+                                    <path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)"></path></g></g>
+                                </svg>
+                <div class="close">
+                    <details @toggle="${this.openChanged}">
+                        <div class="alert-text">
+                            
+                                <span class="alert-message">
+                                    <div class="clearfix text-formatted field field--name-message field--type-text-long field--label-hidden field__item">
+                                        <p>This is a default alert.&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    </div>
+                                </span>
                         </div>
-                    </span>
-                    <svg class="alert-icon" viewBox="0 0 82 82">
-                        <g fill="none" data-name="Path 4286">
-                            <path d="M41 0A41 41 0 110 41 41 41 0 0141 0z"></path>
-                            <path d="M41 6C21.7 6 6 21.7 6 41s15.7 35 35 35 35-15.7 35-35S60.3 6 41 6m0-6c22.644 0 41 18.356 41 41S63.644 82 41 82 0 63.644 0 41 18.356 0 41 0z"></path>
-                        </g>
-                        <g fill="#000321" data-name="Group 3036">
-                            <path d="M35.232 54.188h10.381v7.786H35.232z" data-name="Rectangle 3589"></path>
-                            <path d="M43.378 48.203h-5.854l-3.21-23.669v-4.685h11.81v4.681z" data-name="Path 2763"></path>
-                        </g>
-                    </svg>
-                    <div class="triangle"></div>
-                    <div class="polygon"></div>
+                    </details>
+                </div>
                 </div>
             </div>
         `;
@@ -47,12 +49,22 @@ export class AlertApp extends LitElement {
             display: block;
         }
 
-        .alert-icon {
-            flex: 0 0 90px;
-            height: 90px;
-            margin-left: 0;
-            margin-right: 24px;
+        .alert-card {
+            background: orange;
+            border: 2px solid black;
         }
+
+        .alert-icon {
+            -webkit-box-flex: 0;
+            flex: 0 0 68px;
+            height: 3.35rem;
+            position: relative;
+            display: block;
+            padding: 1% 0.5rem 1% 1rem;
+            top: 0rem;
+            stroke: black;
+        }
+
       `;
     }
 
