e8d9d424c0684695b04c713b4d346958b9ca466f
jhdouglas21 <jhd5244@psu.edu>
2024-03-19 12:48:20 -0400

    Added File haxcms-party-ui

---
+++ src/haxcms-party-ui.js
@@ -0,0 +1,12 @@
+import { LitElement, html, css } from 'lit';
+
+export class HaxCmsPartyUi extends LitElement {
+
+    static get tag() {
+        return 'haxcms-party-ui';
+      }
+
+
+}
+
+globalThis.customElements.define(HaxCmsPartyUi.tag, HaxCmsPartyUi);
