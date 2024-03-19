ec48fff6a791be1f60830e0ebd9c15b698280928
jhdouglas21 <jhd5244@psu.edu>
2024-03-19 13:00:47 -0400

    Imported DDD

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -1,12 +1,29 @@
 import { LitElement, html, css } from 'lit';
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import "@lrnwebcomponents/rpg-character/rpg-character.js";
 
-export class HaxCmsPartyUi extends LitElement {
-
-    static get tag() {
-        return 'haxcms-party-ui';
+export class HaxCmsPartyUi extends DDD {
+  static get styles() {
+    return [
+      super.styles,
+      css`
+      :host {
+        display: block;
       }
+      .my-div {
+        padding: var(-ddd-spacing-5);
+        margin: var(--ddd-spacing-2) var(--ddd-spacing-0);
+        color: var(--ddd-theme-default-keystoneYellow);
+      }
+    `];
+  }
 
-
+  static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String }
+    }
+  }
 }
 
 globalThis.customElements.define(HaxCmsPartyUi.tag, HaxCmsPartyUi);
